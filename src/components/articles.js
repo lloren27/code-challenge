24fd3892c0f141dc49ef9fcc6df3f30593f7a20a
lloren27 <34677045+lloren27@users.noreply.github.com>
2018-10-03 13:55:54 +0200

    Update articles.js

--- src/components/articles.js
+++ src/components/articles.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 
+import './ArticleCard.css';
+
+const ArticleCard = ({ author, excerpt, title }) =>
+  <div className="ArticleCard">
+    <div className="Title">{title}</div>
+    <div className="Author">{author}</div>
+    <div className="Excerpt">{excerpt}</div>
+  </div>;
+
+ArticleCard.propTypes = {
+  author: PropTypes.string.isRequired,
+  excerpt: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default ArticleCard;
