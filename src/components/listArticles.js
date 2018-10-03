7d53182269e5db4f491addcbd6760703528f4915
lloren27 <34677045+lloren27@users.noreply.github.com>
2018-10-03 13:55:15 +0200

    Update listArticles.js

--- src/components/listArticles.js
+++ src/components/listArticles.js
@@ -1,1 +1,40 @@
 
+import React, { Component } from 'react';
+import request from '../../../../infrastructure/request';
+import { ARTICLES_QUERY } from '../../../../infrastructure/request/queries';
+
+import ArticleCard from '../ArticleCard';
+
+class ArticlesList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+    };
+  }
+
+  componentWillMount() {
+    request(ARTICLES_QUERY).then(response => {
+      this.setState({ articles: response.data.articles });
+    });
+  }
+
+  renderCards = articles => articles.map(article => (
+    <ArticleCard key={article.title} {...article} />
+  ));
+
+  render() {
+    const { articles } = this.state;
+    return (
+      <div>
+        {articles.length ?
+          <Grid columns={3} gutter={8} spaceBetween={8}>
+            {this.renderCards(articles)}
+          </Grid> : null
+        }
+      </div>
+    );
+  }
+}
+
+export default ArticlesList;
